81000a79e4352ef5e8dd20b8ef737d061922475b
Alisson Solitto <alisson.solitto@gmail.com>
2019-01-10 14:50:44 -0200

    Versão inicial

---
+++ EnviarEmail/Email.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace EnviarEmail
+{
+    public class Email
+    {
+        private SmtpClient SmtpClient { get; set; }
+
+        public Email (string host, int port, string user, string password)
+        {
+            SmtpClient = new SmtpClient
+            {
+                //Configurações
+                Host = host,
+                Port = port,
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(user, password)
+            };
+        }
+
+        public void SendMail(string from, List<string> lstTo, string subject, string body, bool bodyHtml)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(from);
+
+                    foreach (var email in lstTo)
+                    {
+                        mail.To.Add(new MailAddress(email));
+                    }
+
+                    mail.Subject = subject;
+                    mail.IsBodyHtml = bodyHtml;
+                    mail.Body = body;
+
+                    //Envia E-mail
+                    SmtpClient.Send(mail);                    
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ EnviarEmail/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnviarEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Configurações para o envio de Email
+            var paramEmail = new
+            {
+                host = "", //Host do provedor de Email
+                port = 999, //Porta do provedor de Email
+                user = "", //Email usando para enviar
+                password = "" //Senha do Email
+            };
+
+            //Dados para o envio de Email
+            var dadosEmail = new
+            {
+                from = "", //Remetente
+                lstTo = new List<string> { "element1", "element2", "element3" }, //Lista de destinatários
+                subject = "", //Assunto
+                body = "", //Mensagem
+                bodyHtml = false //A mensagem é um HTML?
+            };
+
+            //Instanciando a classe Email
+            var email = new Email(paramEmail.host, paramEmail.port, paramEmail.user, paramEmail.password);
+            
+            //Usando o método para o envio do Email
+            email.SendMail(dadosEmail.from, dadosEmail.lstTo, dadosEmail.subject, dadosEmail.body, dadosEmail.bodyHtml);
+
+            Console.ReadKey();
+        }
+    }
+}
